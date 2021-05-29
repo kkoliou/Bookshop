f1a3cecf2cdaf81f4fb8476fbfbe9d72fa2bf01d
kkoliou <konstantinoskol@hotmail.com>
2021-05-29 18:45:07 +0300

    Add server service calls

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 const express = require("express")
 const app = express()
 const port = 5050
-app.use('/', express.static('public'))
 
+app.use('/', express.static('public'))
+app.use(express.json())
 app.listen(port, () => console.log(`listening on port ${port}`))
+
+app.put('/favorite', function(req, res) {
+    let data = req.body
+    console.log(data)
+    res.json({
+        status: responses.SUCCESS
+    })
+})
+
+app.delete('/favorite', function(req, res) {
+    let data = req.body
+    console.log(data)
+    res.json({
+        status: responses.SUCCESS
+    })
+})
+
+const responses = {
+	SUCCESS: "success",
+	ALREADY_FAVORITE: "already_favorite",
+	ERROR: "error",
+}
--- public/js/client.js
+++ public/js/client.js
@@ -54,14 +54,67 @@ function favButtonPressed(index) {
     } else {
         addToFavorites(work, function() {
             button.className = "likeBtnFilled"
+        }, function() {
+            button.className = "likeBtn"
+            showAlreadyFavoriteAlert()
         })
     } 
 }
 
-function addToFavorites(work, completionBlock) {
-    completionBlock()
+function addToFavorites(work, completionBlock, alreadyFavoriteBlock) {
+    let init = {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(work)
+    }
+
+    let url = '/favorite'
+    fetch(url, init)
+        .then(response => response.json())
+        .then(data => {
+            if (data.status == responses.SUCCESS) {
+                completionBlock()
+            } else if (data.status == responses.ALREADY_FAVORITE) {
+                alreadyFavoriteBlock()
+            } else {
+                showGenericErrorAlert()
+            }
+        })
 }
 
 function removeFromFavorites(work, completionBlock) {
-    completionBlock()
+    let init = {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(work)
+    }
+
+    let url = '/favorite'
+    fetch(url, init)
+        .then(response => response.json())
+        .then(data => {
+            if (data.status == responses.SUCCESS) {
+                completionBlock()
+            } else {
+                showGenericErrorAlert()
+            }
+        })
+}
+
+function showAlreadyFavoriteAlert() {
+    alert('Already in favorites!')
+}
+
+function showGenericErrorAlert() {
+    alert('Something went wrong. Please try again.')
+}
+
+const responses = {
+	SUCCESS: "success",
+	ALREADY_FAVORITE: "already_favorite",
+	ERROR: "error",
 }
