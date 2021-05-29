69ea29a7b6c2ca9b9348fa8bf5cf5450fd93ad07
kkoliou <konstantinoskol@hotmail.com>
2021-05-29 16:03:17 +0300

    Handle like-button press styling

--- public/js/client.js
+++ public/js/client.js
@@ -1,3 +1,5 @@
+var works = []
+
 function getWorks() {
     let searchInput = document.querySelector("#searchInput").value
     let headers = new Headers()
@@ -16,6 +18,7 @@ function getWorks() {
             if (works.length === 0) {
                 setupEmptyState()
             } else {
+                this.works = works
                 presentWorks(works)
             }
         })
@@ -24,12 +27,14 @@ function getWorks() {
 
 function presentWorks(works) {
     let layout = ""
+    var index = 0
     for (let item of works) {
         layout += `<div class="work-item"><p><b>Title:</b> ${item.titleweb}<br><b>Author:</b> ${item.authorweb}</p>
         <div class="fav">
-        <button id="likeBtn" onclick='favButtonPressed()'></button>
+        <button id="likeBtn${index}" class="likeBtn" onclick='favButtonPressed(${index})'></button>
         </div>
         </div>`
+        index++
     }
     document.querySelector("#works").innerHTML = layout
 }
@@ -38,6 +43,25 @@ function setupEmptyState() {
     document.querySelector("#works").innerHTML = "<p>No results found!</p>"
 }
 
-function favButtonPressed() {
-    
+function favButtonPressed(index) {
+    let button = document.querySelector(`#likeBtn${index}`)
+    let work = works[index]
+
+    if (button.className == "likeBtnFilled") {
+        removeFromFavorites(work, function() {
+            button.className = "likeBtn"
+        })
+    } else {
+        addToFavorites(work, function() {
+            button.className = "likeBtnFilled"
+        })
+    } 
+}
+
+function addToFavorites(work, completionBlock) {
+    completionBlock()
+}
+
+function removeFromFavorites(work, completionBlock) {
+    completionBlock()
 }
